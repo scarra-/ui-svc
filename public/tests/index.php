72f954c18c7c9e609b0f3919aa0cbde122a98286
Karlis Baumanis <karl.baumann@live.de>
2015-03-02 23:50:27 +0200

    pure Angular and some testing

--- public/tests/index.php
+++ public/tests/index.php
@@ -5,11 +5,9 @@
  * Date: 2/27/2015
  * Time: 3:58 PM
  */
-error_reporting(0);
-//die('kaka');
+require_once __DIR__.'/../../.env';
 require('lib/Pusher.php');
-Dotenv::load(__DIR__);
-die('kaka');
+
 
 $app_id = getenv(APP_ID);
 $app_key = getenv(APP_KEY);
@@ -17,15 +15,11 @@ $app_secret = getenv(APP_SECRET);
 
 $pusher = new Pusher($app_key, $app_secret, $app_id);
 
-for ($i = 1; $i <= 30; $i++) {
+for ($i = 1; $i <= 60; $i++) {
 
     sleep(1);
     $data['name'] = 'Karlis';
-    $data['message'] = 'hello waweqwerqworld'.$i;
+    $data['message'] = 'hello waweqwerqworld' . $i;
     $data['time'] = date("l jS \of F Y h:i:s A");
-    print_r($pusher->trigger('test_channel', 'my_event', $data));
+    print_r($pusher->trigger('public_channel', 'new_tweet', $data));
 }
-
-
-
-?>
--- resources/assets/js/my-pusher.js
+++ resources/assets/js/my-pusher.js
@@ -3,17 +3,15 @@ API_KEY = '7b0cc00ab6716c7191b4';
 angular.module('userApp').controller('PusherController', ['$scope', '$pusher', function ($scope, $pusher) {
     var client = new Pusher(API_KEY);
     var pusher = $pusher(client);
-    var my_channel = pusher.subscribe('test_channel');
-
+    var my_channel = pusher.subscribe('public_channel');
     /**
-     * tweets variable should receive history of last 20 tweets when user is connected.
+     * '$scope.tweets' variable should receive history of last 20 tweets when user enters login page.
      * then while he is entering login information, tweet array will be updated with new tweets
      */
-    var tweets = [{name: "Karlis", message: "hello waweqwerqworld19", time: "Monday 2nd of March 2015 07:21:53 PM"},
+    $scope.tweets = [{name: "Karlis", message: "hello waweqwerqworld19", time: "Monday 2nd of March 2015 07:21:53 PM"},
         {name: "Kedwearlis", message: "hello wawwedweworld19", time: "Mwedwedd of March 2015 07:21:53 PM"},
         {name: "Kssdddarlis", message: "dddddhello waweqwerqworld19", time: "Morch 2015 07:21:53 PM"}];
-    $scope.tweets = tweets;
-    my_channel.bind('my_event', function (data) {
+    my_channel.bind('new_tweet', function (data) {
             $scope.tweets.unshift(data);
             //console.log(data);
         }
