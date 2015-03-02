55ec9e2f967f907d4bd328260790db72adf12525
Karlis Baumanis <karl.baumann@live.de>
2015-03-02 22:10:53 +0200

    some layout fixes

--- resources/assets/js/my-pusher.js
+++ resources/assets/js/my-pusher.js
@@ -1,19 +1,20 @@
 API_KEY = '7b0cc00ab6716c7191b4';
 
-var pusherApp = angular.module('userApp');
-
-pusherApp.controller('PusherController', ['$scope', '$pusher', function ($scope, $pusher) {
-    var self = this;
+angular.module('userApp').controller('PusherController', ['$scope', '$pusher', function ($scope, $pusher) {
     var client = new Pusher(API_KEY);
     var pusher = $pusher(client);
     var my_channel = pusher.subscribe('test_channel');
-    var appended = '';
+
+    /**
+     * tweets variable should receive history of last 20 tweets when user is connected.
+     * then while he is entering login information, tweet array will be updated with new tweets
+     */
     var tweets = [{name: "Karlis", message: "hello waweqwerqworld19", time: "Monday 2nd of March 2015 07:21:53 PM"},
         {name: "Kedwearlis", message: "hello wawwedweworld19", time: "Mwedwedd of March 2015 07:21:53 PM"},
         {name: "Kssdddarlis", message: "dddddhello waweqwerqworld19", time: "Morch 2015 07:21:53 PM"}];
     $scope.tweets = tweets;
     my_channel.bind('my_event', function (data) {
-            $scope.tweets.push(data);
+            $scope.tweets.unshift(data);
             //console.log(data);
         }
     );
