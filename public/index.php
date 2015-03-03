d95d70c23f6f6e85ac7d8666378e7cbb76920ec9
Martins Sipenko <martins.sipenko@gmail.com>
2015-03-03 15:55:37 +0200

    Fixed styling

--- public/index.php
+++ public/index.php
@@ -1,20 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Karl
- * Date: 3/2/2015
- * Time: 11:08 AM
- */
-// web/index.php
-require_once __DIR__ . '/../vendor/autoload.php';
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../resources/views',
 ));
-
 
 $app['twig']->addFunction(new Twig_SimpleFunction('elixir', function ($file){
     static$manifest = null;
@@ -37,10 +29,6 @@ $app->get('/', function () use ($app) {
     $response = new \Symfony\Component\HttpFoundation\Response($return);
 
     $response->setTtl(5);
-
-
-    //var_dump($response);
-
 
     return $response;
 
