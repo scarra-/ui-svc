90c82afb4e2a11c2dfe06038037c7a483d324633
Reinholds <reinholdspirags@gmail.com>
2015-03-03 16:41:57 +0200

    add correct logged in showing profile partial

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,14 +3,33 @@ require('angular-resource/angular-resource.min');
 require('angular-route/angular-route.min');
 
 
-
-
 var userApp = angular.module('userApp', ['ngRoute', 'ngResource', 'pusher-angular'])
     .factory('UserService', ['$resource', function($resource) {
         return $resource(window.userServiceUrl+'/users/:id', {id:'@id'}, {
             update: {method: 'PUT'}
         });
     }])
+    .factory('LoggedInService', [function() {
+        var isLoggedIn = false;
+        return {
+            isLoggedIn: function() {
+                return isLoggedIn;
+            },
+            login: function() {
+                console.log("login caled");
+                isLoggedIn = true;
+            },
+            logout: function() {
+                isLoggedIn = false;
+            }
+        }
+    }])
+
+    .controller('MainController', ['LoggedInService', function(LoggedInService) {
+        var self = this;
+        self.auth = LoggedInService.isLoggedIn;
+    }])
+
     .controller('RegisterController', ['$scope', 'UserService', function($scope, UserService) {
         var self = this;
 
@@ -25,13 +44,14 @@ var userApp = angular.module('userApp', ['ngRoute', 'ngResource', 'pusher-angula
             });
         };
     }])
-    .controller('LoginController', ['$http', function($http) {
+    .controller('LoginController', ['$http', 'LoggedInService', function($http, LoggedInService) {
         var self = this;
 
         self.login = function() {
             console.log(self.user);
             $http.post(window.userServiceUrl+'/authenticate', self.user).then(function(response) {
                window.sessionStorage.token = response.data.token;
+               LoggedInService.login();
 
             }, function(errorResponse) {
                 console.log(errorResponse.data);
@@ -44,21 +64,12 @@ var userApp = angular.module('userApp', ['ngRoute', 'ngResource', 'pusher-angula
         self.sendMessage = function() {
             console.log(self.msg);
             $http.post(window.contentServiceUrl+'/messages', self.msg).then(function() {gi
-              
+
 
             }, function(errorResponse) {
                 console.log(errorResponse.data);
             });
         };
-    }])
-    .controller('AuthController', ['$scope', function($scope) {
-        $scope.auth = function(user) {
-          console.log(user.bool);
-        };
     }]);
 require('pusher-angular');
 require('./my-pusher');
-
-
-
-
