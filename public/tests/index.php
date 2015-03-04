4a941ee8459990e72dec56b21eacd9f71f616675
Reinholds <reinholdspirags@gmail.com>
2015-03-04 10:28:36 +0200

    add new .env

--- public/tests/index.php
+++ public/tests/index.php
@@ -9,9 +9,14 @@ require_once __DIR__.'/../../.env';
 require('lib/Pusher.php');
 
 
-$app_id = getenv(APP_ID);
-$app_key = getenv(APP_KEY);
-$app_secret = getenv(APP_SECRET);
+// $app_id = getenv(APP_ID);
+// $app_key = getenv(APP_KEY);
+// $app_secret = getenv(APP_SECRET);
+
+$app_id = $_ENV['UI_SVC_APP_ID'];
+$app_key = $_ENV['UI_SVC_APP_KEY'];
+$app_secret = $_ENV['UI_SVC_APP_SECRET'];
+
 
 $pusher = new Pusher($app_key, $app_secret, $app_id);
 
@@ -19,7 +24,7 @@ for ($i = 1; $i <= 40; $i++) {
 
     sleep(1);
     $data['name'] = 'Karlis';
-    $data['message'] = 'hello waweqwerqworld' . $i;
+    $data['message'] = 'hello world' . $i;
     $data['time'] = date("l jS \of F Y h:i:s A");
     print_r($pusher->trigger('public_channel', 'new_tweet', $data));
 }
