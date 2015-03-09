39217ca64ec5c671fbb6a4d29fb28e2630da9c96
Reinholds <reinholdspirags@gmail.com>
2015-03-09 15:15:07 +0200

    add post request to subscription service

--- resources/assets/js/controllers/UserModalController.js
+++ resources/assets/js/controllers/UserModalController.js
@@ -5,15 +5,23 @@ angular.module('messageApp.UserModalController', [])
         'AuthService',
         'UserService',
         'AppConfig',
-        function ($http, $routeParams, AuthService, UserService, AppConfig) {
+        'localStorageService',
+        function ($http, $routeParams, AuthService, UserService, AppConfig, localStorage) {
             var self = this;
 
             self.auth = AuthService.isLoggedIn;
             self.user = UserService.get({id:$routeParams.username}, function() {});
 
+            var authHeader = { 'Authorization': 'Bearer ' + localStorage.get('token') };
+
             self.followUser = function () {
                 console.log('follow user: ' + self.user.login);
-                $http.post(AppConfig.subscriptionServiceUrl+'/subscribe').then(function(success) {
+                $http.post(
+                    AppConfig.subscriptionServiceUrl + '/subscribe',
+                    {"username" : $routeParams.username},
+                    {headers : authHeader}
+                )
+                .then(function(success) {
                     console.log(success.data);
                 }, function(failure) {
                     console.log(failure.data);
