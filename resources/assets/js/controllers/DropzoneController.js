3bb3a0b9473f697e3271ed135ddf3c30b6ea8bfd
Martins Sipenko <martins.sipenko@gmail.com>
2015-03-11 09:55:35 +0200

    Another way of uploading a file

--- resources/assets/js/controllers/DropzoneController.js
+++ resources/assets/js/controllers/DropzoneController.js
@@ -3,21 +3,5 @@ angular.module('messageApp.DropzoneController',[])
 
       var self = this;
 
-      self.dropzoneConfig = {
-        'options': { // passed into the Dropzone constructor
-          'url': 'upload.php',
-          'autoProcessQueue': false
-        },
-        'eventHandlers': {
-          'sending': function (file, xhr, formData) {
-             console.log("sending the file");
-             // can add token if needed to xhr
-          },
-          'success': function (file, response) {
-              console.log("successful upload");
-              console.log(response);
 
-          }
-        }
-      };
   }]);
