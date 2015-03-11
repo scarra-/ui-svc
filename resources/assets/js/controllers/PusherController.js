01d0f841534846bd433ca171b30198e47bce5f79
Reinholds <reinholdspirags@gmail.com>
2015-03-11 09:27:01 +0200

    add auth header for GET request to /messages

--- resources/assets/js/controllers/PusherController.js
+++ resources/assets/js/controllers/PusherController.js
@@ -1,33 +1,41 @@
 angular.module('messageApp.PusherController', ['AppConfig'])
-    .controller('PusherController', [ 'StreamService', 'AppConfig', '$pusher', '$http', function (StreamService, AppConfig, $pusher, $http) {
-        var self = this;
+    .controller('PusherController', [
+        'StreamService',
+        'AppConfig',
+        '$pusher',
+        '$http',
+        'localStorageService',
+        function (StreamService, AppConfig, $pusher, $http, localStorage) {
+            var self = this;
 
-        self.pusherMessages = StreamService.getPusherMessages;
-        self.contentMessages = StreamService.getContentMessages;
-        var loading = false;
+            self.pusherMessages = StreamService.getPusherMessages;
+            self.contentMessages = StreamService.getContentMessages;
+            var loading = false;
 
-        self.page = AppConfig.contentServiceUrl+'/messages';
-        var parse = require('parse-link-header');
+            self.page = AppConfig.contentServiceUrl+'/messages';
+            var parse = require('parse-link-header');
+            var authHeader = { 'Authorization': 'Bearer '+ localStorage.get('token') };
 
-        self.myPagingFunction = function() {
-            if (self.page !== false && loading === false) {
-                loading = true;
-                $http.get(self.page).then(function(success) {
-                    var pagination = parse(success.headers('link'));
 
-                    if (pagination.next) {
-                        self.page = pagination.next.url;
-                    } else {
-                        self.page = false;
-                    }
+            self.myPagingFunction = function() {
+                if (self.page !== false && loading === false) {
+                    loading = true;
+                    $http.get(self.page, {headers: authHeader}).then(function(success) {
+                        var pagination = parse(success.headers('link'));
 
-                    angular.forEach(success.data, function(message) {
-                        StreamService.addContentMessage(message);
+                        if (pagination.next) {
+                            self.page = pagination.next.url;
+                        } else {
+                            self.page = false;
+                        }
+
+                        angular.forEach(success.data, function(message) {
+                            StreamService.addContentMessage(message);
+                        });
+                        loading = false;
+                    }, function (failure) {
+                        loading = false;
                     });
-                    loading = false;
-                }, function (failure) {
-                    loading = false;
-                });
-            }
-        };
+                }
+            };
     }]);
