c7622296b3fff5f93afc94de20ea8fd29118d23e
ANiedritis <andris.niedritis@gmail.com>
2015-03-12 11:21:17 +0200

    send message button is disabled after pressing it once, while it does its job it shows loading

--- resources/assets/js/controllers/MsgController.js
+++ resources/assets/js/controllers/MsgController.js
@@ -2,10 +2,16 @@ angular.module('messageApp.MsgController', ['AppConfig'])
     .controller('MsgController', ['$http', 'AppConfig','localStorageService', '$upload', function($http, AppConfig, localStorage, $upload) {
         var self = this;
         self.img = {};
+        self.buttonText = 'Send message';
+        self.disabled   = false;
 
         var authHeader = { 'Authorization': 'Bearer ' + localStorage.get('token') };
 
         self.submit = function() {
+            self.internalError = false;
+            self.disabled   = true;
+            self.buttonText = 'Loading...';
+
             if (self.img.image) {
                 console.log(self.img.image);
                 $upload.upload({
@@ -19,11 +25,25 @@ angular.module('messageApp.MsgController', ['AppConfig'])
                     console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
                     self.msg.image_id = data.image_id;
                     self.sendMessage();
+                        self.buttonText = 'Send message';
+                        self.disabled   = false;
                 }).error(function (data, status, headers, config) {
-                    console.log('Failed to upload image: ' + config.file[0].name);
+                    // if(status=='0'){
+                    //     self.buttonText = 'Send message';
+                    //     self.disabled   = false;
+                    //     self.internalError = true;
+                    // }
+                    // else{
+                        self.buttonText = 'Send message';
+                        self.disabled   = false;
+                        console.log('Failed to upload image: ' + config.file[0].name);
+                    //}
+
                 });
             } else {
                 self.sendMessage();
+                self.buttonText = 'Send message';
+                self.disabled   = false;
             }
         };
 
